de36a2714bdc872ec182820680cb1433733c499a
vinaysidda <vinaysiddesh.siddesh@gmail.com>
2025-03-26 12:38:44 -0400

    today class commit

--- eval_pro/settings.py
+++ eval_pro/settings.py
@@ -28,7 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["ff76-72-75-216-214.ngrok-free.app",
 "127.0.0.1",
-"localhost",]
+"localhost",
+"djangobackend-dkfmgvadc4crdphy.canadacentral-01.azurewebsites.net"
+]
 
 
 CORS_ALLOWED_ORIGINS = [
